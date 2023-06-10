a4f037a16353749bba014343873a04f5ca86c6d2
John Modica <39076455+CybernetiX-S3C@users.noreply.github.com>
2023-06-10 18:40:42 -0400

    Update Bard.py

--- Bard.py
+++ Bard.py
@@ -2,67 +2,61 @@ import requests
 import logging
 import unittest
 
-
 def connect_to_bard(bard_url):
-    """Connects to Google Bard and returns a response object."""
-    logging.info("Connecting to Google Bard...")
-    response = requests.get(bard_url)
-    logging.info("Connected to Google Bard.")
-    if response.status_code == 200:
-        return response
-    else:
-        raise Exception("Could not connect to Google Bard: {}".format(response.status_code))
-
+  """Connects to Google Bard and returns a response object."""
+  logging.info("Connecting to Google Bard...")
+  response = requests.get(bard_url)
+  logging.info("Connected to Google Bard.")
+  if response.status_code == 200:
+    return response
+  else:
+    raise Exception("Could not connect to Google Bard: {}".format(response.status_code))
 
 def get_bard_response(bard_url, query):
-    """Sends a query to Google Bard and returns the response."""
-    logging.info("Sending query to Google Bard...")
-    response = connect_to_bard(bard_url)
-    logging.info("Query sent to Google Bard.")
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception("Could not get a response from Google Bard: {}".format(response.status_code))
-
+  """Sends a query to Google Bard and returns the response."""
+  logging.info("Sending query to Google Bard...")
+  response = connect_to_bard(bard_url)
+  logging.info("Query sent to Google Bard.")
+  if response.status_code == 200:
+    return response.json()
+  else:
+    raise Exception("Could not get a response from Google Bard: {}".format(response.status_code))
 
 def main():
-    """The main function."""
+  """The main function."""
+  # Set the Bard URL.
+  bard_url = "https://bard.google.com/v1/query"
+
+  # Set the query.
+  query = "What is the capital of France?"
+
+  # Get the Bard response.
+  response = get_bard_response(bard_url, query)
+
+  # Print the Bard response.
+  logging.info("Bard response: {}".format(response))
+
+if __name__ == "__main__":
+  # Run the main function.
+  main()
+
+class TestConnectToBard(unittest.TestCase):
+  """Tests the connect_to_bard function."""
+
+  def test_connect_to_bard_success(self):
+    """Tests that connect_to_bard succeeds."""
     # Set the Bard URL.
     bard_url = "https://bard.google.com/v1/query"
 
-    # Set the query.
-    query = "What is the capital of France?"
+    # Make a request to Google Bard.
+    response = connect_to_bard(bard_url)
 
-    # Get the Bard response.
-    response = get_bard_response(bard_url, query)
-
-    # Print the Bard response.
-    logging.info("Bard response: {}".format(response))
-
+    # Assert that the response was successful.
+    self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
-    # Configure logging.
-    logging.basicConfig(level=logging.INFO)
-
-    # Run the main function.
-    main()
+  # Run the unit tests.
+  unittest.main()
 
 
-class TestConnectToBard(unittest.TestCase):
-    """Tests the connect_to_bard function."""
 
-    def test_connect_to_bard_success(self):
-        """Tests that connect_to_bard succeeds."""
-        # Set the Bard URL.
-        bard_url = "https://bard.google.com/v1/query"
-
-        # Make a request to Google Bard.
-        response = connect_to_bard(bard_url)
-
-        # Assert that the response was successful.
-        self.assertEqual(response.status_code, 200)
-
-
-if __name__ == "__main__":
-    # Run the unit tests.
-    unittest.main()
