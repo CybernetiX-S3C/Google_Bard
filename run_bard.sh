cdbab0fe79c69716270e4206f461440269f72aa9
John Modica <39076455+CybernetiX-S3C@users.noreply.github.com>
2023-06-11 02:10:26 -0400

    Update run_bard.sh

--- run_bard.sh
+++ run_bard.sh
@@ -1,1 +1,26 @@
 
+#!/bin/bash
+
+# Install the Google Bard CLI tool
+
+sudo apt-get install google-bard
+
+# Create a directory for your Bard projects
+
+mkdir ~/bard
+
+# Change to the Bard directory
+
+cd ~/bard
+
+# Initialize a new Bard project
+
+bard init
+
+# Start the Bard server
+
+bard server
+
+# Access Bard in the terminal
+
+bard
